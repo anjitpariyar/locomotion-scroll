54e10ae8e33a78c9d14d071b0cedbaa0ecbbcc49
anjitzero <45827003+anjitzero@users.noreply.github.com>
2021-02-08 12:40:53 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import LocomotiveScroll from './node_modules/locomotive-scroll/dist/locomotive-scroll.esm.js';
+
 
 const scroll = new LocomotiveScroll({
      el: document.querySelector('[data-scroll-container]'),
